1e064da325b29a896808f0af12169d366682ada8
davidMadueke <116285669+davidMadueke@users.noreply.github.com>
2025-08-24 08:34:27 +0100

    feat [SWE]: Created LED Ring HW Unit Test

--- software/Simple Transport Midi Controller/lib/HAL/LedRing.cpp
+++ software/Simple Transport Midi Controller/lib/HAL/LedRing.cpp
@@ -1,32 +1,33 @@
 #include "LedRing.h"
 
 LedRing::LedRing(uint8_t I2C_LedRing_Addr) {
-    LedRing = new LEDRingSmall(I2C_LedRing_Addr);
+    Ring = new LEDRingSmall(I2C_LedRing_Addr);
+    startLedValue = 0;
 }
 
-LedRing::setup() {
-    LedRing->LEDRingSmall_Reset();
+void LedRing::setup() {
+    Ring->LEDRingSmall_Reset();
   delay(20);
 
-  LedRing->LEDRingSmall_Configuration(0x01); //Normal operation
-  LedRing->LEDRingSmall_PWMFrequencyEnable(1);
-  LedRing->LEDRingSmall_SpreadSpectrum(0b0010110);
-  LedRing->LEDRingSmall_GlobalCurrent(0x04);
-  LedRing->LEDRingSmall_SetScaling(0xF0);
-  LedRing->LEDRingSmall_PWM_MODE();
+  Ring->LEDRingSmall_Configuration(0x01); //Normal operation
+  Ring->LEDRingSmall_PWMFrequencyEnable(1);
+  Ring->LEDRingSmall_SpreadSpectrum(0b0010110);
+  Ring->LEDRingSmall_GlobalCurrent(0x04);
+  Ring->LEDRingSmall_SetScaling(0xF0);
+  Ring->LEDRingSmall_PWM_MODE();
 }
 
-LedRing::reset(){
-    LedRing->LEDRingSmall_reset();
+void LedRing::reset(){
+    Ring->LEDRingSmall_Reset();
 }
 
-LedRing::setLedStartValue(uint8_t startValue){
+void LedRing::setLedStartValue(uint8_t startValue){
     startLedValue = startValue;
 }
 
-void setLed(uint8_t logicalLEDPosition, uint8_t r, uint8_t g, uint8_t b){
+void LedRing::setLed(uint8_t logicalLEDPosition, uint8_t r, uint8_t g, uint8_t b){
     int actualLedPosition = getLEDPosition(logicalLEDPosition);
-    LEDRingSmall.LEDRingSmall_Set_RED(actualLedPosition, r);
-    LEDRingSmall.LEDRingSmall_Set_GREEN(actualLedPosition, g);
-    LEDRingSmall.LEDRingSmall_Set_BLUE(actualLedPosition, b);
+    Ring->LEDRingSmall_Set_RED(actualLedPosition, r);
+    Ring->LEDRingSmall_Set_GREEN(actualLedPosition, g);
+    Ring->LEDRingSmall_Set_BLUE(actualLedPosition, b);
 }
