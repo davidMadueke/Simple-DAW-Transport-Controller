2f4236c8b9dd2496af75f7231ec0e8a5f17161a1
davidMadueke <116285669+davidMadueke@users.noreply.github.com>
2025-08-31 08:37:52 +0100

    feat [SWE]: Added VOL_ENCODER_MIDI_STATE

---
+++ software/Simple Transport Midi Controller/lib/states/VOL_ENCODER_MIDI_STATE.h
@@ -0,0 +1,16 @@
+struct VOL_ENCODER_MIDI_STATE {
+    
+    VOL_ENCODER_MODE mode;
+    uint8_t ledRing_Position;
+    bool SW_pressEvent;
+    uint8_t SW_numOfPresses;
+    uint8_t SW_longPress;
+    uint8_t POT_value;
+};
+
+enum VOL_ENCODER_MODE 
+{
+    MODE1;
+    MODE2;
+    MODE3;
+};
