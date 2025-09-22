9b92df043a82ba13e8bc97ecee6d60dba7a47dd1
davidMadueke <116285669+davidMadueke@users.noreply.github.com>
2025-09-22 22:59:23 +0100

    fix [SWE]: Refactor MIDI_Button object
    We extract the Toggle ON/OFF Led State, the SinglePress Led inicator State and the LongPress Led States to their own methods that can be called in the child button constructor - allowing us to customise the internal LED State functionality of each child Button

--- software/Simple Transport Midi Controller/lib/Buttons/MIDI_Button.h
+++ software/Simple Transport Midi Controller/lib/Buttons/MIDI_Button.h
@@ -5,31 +5,50 @@
 // Define the base class MIDI_Button
 class MIDI_Button {
 protected:
-    Button* button;
     uint8_t m_LONG_PRESS_ms;
+
+    // Setup stored LED States struct for different types of buttons to be setup
     BUTTON_LED_STATE* longPressLedState;
     BUTTON_LED_STATE* singlePressLedIndicator;
+    BUTTON_LED_STATE* toggleLedState;
+    bool m_noLedIndicator;
 
 public:
-
-    MIDI_Button(uint8_t i2cAddr, uint32_t dbTime, BUTTON_LED_STATE* longPressState, BUTTON_LED_STATE* singlePressState) {
+    Button* button;
+    MIDI_Button(uint8_t i2cAddr, uint32_t dbTime) {
         button = new Button(i2cAddr, dbTime);
-        longPressLedState = longPressState;
-        singlePressLedIndicator = singlePressState;
+        m_noLedIndicator = true;
+        
     }
 
     virtual ~MIDI_Button() {}
 
     virtual void begin(uint8_t brightnessPrescaler,
-        uint8_t multiPressTimer = 150, uint8_t timeForLongPress = 1000) {
+        uint8_t multiPressTimer = 150) {
+        setBrightness(brightnessPrescaler);
+        button->setMultiPressTimer(multiPressTimer);
+    }
+
+    // Setup an SinglePressLedIndicator begin
+    virtual void setup_singlePressLedIndicator(BUTTON_LED_STATE* singlePressState){
+        m_noLedIndicator = false;
+        singlePressLedIndicator = singlePressState;
         setSinglePressLedIndicator(
             singlePressLedIndicator->Red,
             singlePressLedIndicator->Green,
             singlePressLedIndicator->Blue
         );
-        setBrightness(brightnessPrescaler);
-        button->setMultiPressTimer(multiPressTimer);
+
+    }
+
+    // Setup a LongPressLedState begin
+    virtual void setup_longPress(BUTTON_LED_STATE* longPressState, uint8_t timeForLongPress = 1000 ){
         setTimeForLongPress(timeForLongPress);
+        longPressLedState = longPressState;
+    }
+
+    virtual void setup_toggleLedState(BUTTON_LED_STATE* toggleLedState){
+        toggleLedState = toggleLedState;
     }
 
     virtual void setBrightness(uint8_t brightnessPrescaler) { button->setBrightness(brightnessPrescaler); }
@@ -37,8 +56,10 @@ public:
     virtual void setLedColour(uint8_t r, uint8_t g, uint8_t b) { button->setLedColour(r, g, b); }
 
     virtual void toggleLedOn() {
-        setLedColour(singlePressLedIndicator);
+        setLedColour(toggleLedState);
     }
+
+    virtual void ledOff(){ button->ledOff(); }
 
     virtual void setLedColour(BUTTON_LED_STATE* state) {
         setLedColour(
@@ -48,9 +69,15 @@ public:
         );
     }
 
-    virtual void setSinglePressLedIndicator(uint8_t r, uint8_t g, uint8_t b) { button->setSinglePressLedIndicator(r, g, b); }
+    virtual void setSinglePressLedIndicator(uint8_t r, uint8_t g, uint8_t b) { 
+        if(!m_noLedIndicator){
+            button->setSinglePressLedIndicator(r, g, b); 
+        }
+    }
 
     virtual void setTimeForLongPress(uint8_t time_ms) { m_LONG_PRESS_ms = time_ms; }
+
+    virtual uint8_t getTimeForLongPress(){ return m_LONG_PRESS_ms }
 
     // Virtual stateMachine to be overridden
     virtual void stateMachine(bool ISR_State, BUTTON_HAL_STATE* HAL) = 0;
--- software/Simple Transport Midi Controller/lib/Buttons/PlayButton.h
+++ software/Simple Transport Midi Controller/lib/Buttons/PlayButton.h
@@ -16,17 +16,24 @@ public:
         toSINGLE_PRESS
     };
     
-    PlayButton(uint8_t i2cAddr, uint32_t dbTime, BUTTON_LED_STATE* longPressState, BUTTON_LED_STATE* singlePressState)
-        : MIDI_Button(i2cAddr, dbTime, longPressState, singlePressState) {}
+    PlayButton(uint8_t i2cAddr, uint32_t dbTime, BUTTON_LED_STATE* longPressState, 
+        BUTTON_LED_STATE* singlePressState, uint8_t timeForLongPress = 100)
+        : MIDI_Button(i2cAddr, dbTime) 
+        {
+            setup_singlePressLedIndicator(singlePressState);
+            setup_longPress(longPressState, timeForLongPress);
+        };
 
     // Override stateMachine
     void stateMachine(bool ISR_State, BUTTON_HAL_STATE* HAL) override {
         static ButtonMode MODE;
         uint8_t numOfPresses = button->multiPressRead(ISR_State);
+        if (numOfPresses > 0){ HAL->buttonPressEvent = true; } // We will need to handle turning off the press event as soon as MIDI msg is compiled
+
         switch (MODE) {
             case SINGLE_PRESS:
                 HAL->numOfPresses = numOfPresses;
-                if (button->pressedFor(m_LONG_PRESS_ms)) {
+                if (button->pressedFor(getTimeForLongPress())) {
                     MODE = toLONG_PRESS;
                     HAL->longPress = true;
                     HAL->numOfPresses = 1;
@@ -38,7 +45,7 @@ public:
                     MODE = LONG_PRESS;
                 break;
             case LONG_PRESS:
-                if (button->pressedFor(LONG_PRESS)) {
+                if (button->pressedFor(getTimeForLongPress())) {
                     HAL->longPress = false;
                     MODE = toSINGLE_PRESS;
                 }
