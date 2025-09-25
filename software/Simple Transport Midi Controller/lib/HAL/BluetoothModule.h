0fd0ffb7d75afda17558e392ee17e1e9ea73c8a4
davidMadueke <116285669+davidMadueke@users.noreply.github.com>
2025-09-25 01:28:44 +0100

    feat [SWE]: Created BLE Transceiver MIDI Module

---
+++ software/Simple Transport Midi Controller/lib/HAL/BluetoothModule.h
@@ -0,0 +1,12 @@
+#include <PushButton.h>
+
+class BluetoothModule
+{
+    private:
+        PushButton* EnableButton;
+    
+    public:
+        BluetoothModule(uint8_t EnablePin, uint8_t LedPin){
+
+        };
+};
