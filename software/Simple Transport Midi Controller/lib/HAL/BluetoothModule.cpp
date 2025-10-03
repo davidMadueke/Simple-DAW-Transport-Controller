5f145f1ce8be6cca3139f8cff75a1a06dd4578e8
davidMadueke <116285669+davidMadueke@users.noreply.github.com>
2025-10-04 00:58:39 +0100

    feat [SWE} Created BLE Module Button State Machine

--- software/Simple Transport Midi Controller/lib/HAL/BluetoothModule.cpp
+++ software/Simple Transport Midi Controller/lib/HAL/BluetoothModule.cpp
@@ -1,4 +1,5 @@
 #include "BluetoothModule.h"
+#include <BLUETOOTH_MIDI_STATE.h>
 
 void BluetoothModule::digitalLedWrite(uint8_t value){
     if (ledWriteCallback) {
@@ -7,4 +8,34 @@ void BluetoothModule::digitalLedWrite(uint8_t value){
                 Serial.println("Error: ledWriteCallback is not set.");
                 return;
             }
+};
+
+void BluetoothModule::buttonStateMachine(bool button_ISR_State, BLUETOOTH_MIDI_STATE* state){
+    PushButton* button = getBluetoothPushButton();
+    
+
+    switch (MODE)
+    {
+    case BT_OFF:
+        if (button->pressedFor(m_timeForLongPress_ms)){
+            MODE = START_SCANNING;
+            break;
+        }
+        break;
+    
+    case START_SCANNING:
+        break;
+    
+    case BLE_ENABLED:
+        break;
+    
+    case toBT_OFF:
+        break;
+
+    default:
+        Serial.println("Undefined Bluetooth Module State Machine Behaviour");
+        break;
+    }
+
+
 }
--- software/Simple Transport Midi Controller/lib/HAL/BluetoothModule.h
+++ software/Simple Transport Midi Controller/lib/HAL/BluetoothModule.h
@@ -1,13 +1,18 @@
 #include <PushButton.h>
+#include <BLUETOOTH_MIDI_STATE.h>
 
 class BluetoothModule
 {
     private:
-        PushButton* EnableButton;
+        PushButton* EnableButton = nullptr;
 
         uint8_t m_enablePin, m_ledPin;
+        uint8_t m_timeForLongPress_ms = 150;
 
         void (*ledWriteCallback)(uint8_t, uint8_t) = nullptr;
+        void (*BLE_MidiCallback)() = nullptr;
+
+        bool isConnected = false;
     
     public:
         BluetoothModule(uint8_t EnablePin, uint8_t LedPin){
@@ -18,10 +23,37 @@ class BluetoothModule
 
         void update();
 
+        void setTimeForLongPress(uint8_t time_ms) { m_timeForLongPress_ms = time_ms; };
+        uint8_t getTimeForLongPress(){ return m_timeForLongPress_ms; };
+        
         void setDigitalLedWriteCallback(void (*digitalWrite)(uint8_t, uint8_t)){
             ledWriteCallback = digitalWrite;
         }
 
         void digitalLedWrite(uint8_t value);
+        
+        PushButton* getBluetoothPushButton(){
+            if(EnableButton){
+                return EnableButton;
+            }
+            
+            Serial.println("Bluetooth Button not created");
+            return nullptr;
+        };
+
+        enum ButtonMode{
+            /* We require that as soon as a long press is detected, the BLE module starts to scan for devices
+            When another long press is enabled then we need the BLE device to stop all scans and current connections*/
+            BT_OFF,
+            START_SCANNING,
+            BLE_ENABLED,
+            toBT_OFF,
+        };
+
+        ButtonMode MODE = BT_OFF;
+
+        void buttonStateMachine(bool button_ISR_State, BLUETOOTH_MIDI_STATE* state);
+
+        // A method that sets the
 
 };
