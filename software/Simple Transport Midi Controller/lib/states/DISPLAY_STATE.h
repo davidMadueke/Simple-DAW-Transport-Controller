c1df3b958173bc5280e4701da76828b3adfcb8cb
davidMadueke <116285669+davidMadueke@users.noreply.github.com>
2026-07-05 20:12:13 +0100

    feat [SWE]: Added DISPLAY_STATE Struct

--- software/Simple Transport Midi Controller/lib/states/DISPLAY_STATE.h
+++ software/Simple Transport Midi Controller/lib/states/DISPLAY_STATE.h
@@ -1,22 +1,9 @@
+#include <Arduino.h>
 #include <INF_SCROLL_MIDI_STATE.h>
 #include <VOL_ENCODER_MIDI_STATE.h>
 #include <BLUETOOTH_MIDI_STATE.h>
 
-struct  DISPLAY_STATE {
-    INF_SCROLL_MODE INF_SCROLL_MODE;
-    unsigned char INF_SCROLL_MODE_Value;
-    
-    bool VOL_POT_Event;
-    VOL_ENCODER_MODE VOL_ENCODER_MODE;
-    unsigned char VOL_POT_Value;
-
-    unsigned char DAW_TEMPO;
-    bool TAP_TEMPO_Event;
-
-    BATTERY_DISPLAY_STATE BATTERY_Stats;
-
-    BLUETOOTH_MIDI_STATE BLE_Flags;
-};
+#pragma once
 
 struct BATTERY_DISPLAY_STATE {
     bool isLowPower;
@@ -24,3 +11,24 @@ struct BATTERY_DISPLAY_STATE {
     bool noBatteryConnected;
     float cellPercentage;
 };
+struct  DISPLAY_STATE {
+    uint32_t version = 0;
+    INF_SCROLL_MODE infScrollMode;
+    unsigned char infScrollMode_value;
+    
+    VOL_ENCODER_MODE volEncoderMode;
+    unsigned char volEncoder_potValue;
+
+    unsigned char dawTempo;
+    bool tapTempoEvent;
+
+    BATTERY_DISPLAY_STATE batteryState;
+
+    BLUETOOTH_MIDI_STATE bleFlags;
+
+    bool dirty(const DISPLAY_STATE& other) const { return version != other.version; }
+};
+
+
+extern DISPLAY_STATE g_displayState;
+extern portMUX_TYPE g_displayStateMux;
